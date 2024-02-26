f0c7f8aa2e6357dcf4e95a25ab5f11919be79148
Jack (Jacques) Fork <hex@hexdev.xyz>
2024-02-26 16:09:58 +0000

    Navbar

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,8 @@
 <head>
     <title><?= $page ?> - Bean & Brew</title>
     <link rel="stylesheet" href="./assets/style.css">
+    <script src="https://kit.fontawesome.com/26225932ca.js" crossorigin="anonymous"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
+    </style>
 </head>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,17 @@
-<div class="py-4 px-72 bg-orange-400 text-white flex justify-between items-center">
+<div class="navbar">
     <div class="transition absolute hover:brightness-90">
-        <h1>Bean & Brew</h1>
+        <h1 class="text-lg"><i class="text-xl fa-solid fa-mug-hot"></i> Bean & Brew</h1>
     </div>
     <span></span>
     <div>
-        <a class="navButton" href="/Bean-And-Brew/index.php">Home</a>
         <a class="navButton" href="/Bean-And-Brew/menu.php">Menu</a>
-        <a class="navButton" href="/Bean-And-Brew/about.php">About</a>
-        <a class="navButton" href="/Bean-And-Brew/contact.php">Contact</a>
+        <a class="navButton" href="/Bean-And-Brew/sessions.php">Baking Lessons</a>
+        <?php if (!isset($_SESSION['user'])) { ?>
+        <a class="navButton navSecondary" href="/Bean-And-Brew/makeanorder.php">Start Order <i class="ml-2 text-sm fa-solid fa-circle-plus"></i></a>
+        <a class="navButton navPrimary" href="/Bean-And-Brew/account.php">My Account  <i class="text-sm ml-2 fa-regular fa-user"></i></a>
+        <?php } else { ?>
+        <a class="navButton navSecondary" href="/Bean-And-Brew/store-locator.php">Find a Cafe <i class="text-sm fa-solid fa-location-dot"></i></a>
+        <a class="navButton navPrimary" href="/Bean-And-Brew/contact.php">Member Login</a>
+        <?php } ?>
     </div>
 </div>
