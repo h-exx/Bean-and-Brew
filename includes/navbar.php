37de83e5111121d5978c7638d0858ca2f90d2dce
Jack (Jacques) Fork <hex@hexdev.xyz>
2024-02-27 10:20:29 +0000

    commented on navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,12 +6,12 @@
     <div>
         <a class="navButton" href="/Bean-And-Brew/menu.php">Menu</a>
         <a class="navButton" href="/Bean-And-Brew/sessions.php">Baking Lessons</a>
-        <?php if (!isset($_SESSION['user'])) { ?>
+    <?php if (isset($_SESSION['user'])) { // if the user is logged in, show these buttons below?>
         <a class="navButton navSecondary" href="/Bean-And-Brew/makeanorder.php">Start Order <i class="ml-2 text-sm fa-solid fa-circle-plus"></i></a>
         <a class="navButton navPrimary" href="/Bean-And-Brew/account.php">My Account  <i class="text-sm ml-2 fa-regular fa-user"></i></a>
-        <?php } else { ?>
+    <?php } else { // if not, show these ones instead ?>
         <a class="navButton navSecondary" href="/Bean-And-Brew/store-locator.php">Find a Cafe <i class="text-sm fa-solid fa-location-dot"></i></a>
         <a class="navButton navPrimary" href="/Bean-And-Brew/contact.php">Member Login</a>
-        <?php } ?>
+    <?php } ?>
     </div>
 </div>
